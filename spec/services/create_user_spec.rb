e20976024b96bab1cd213ddf12642daa2bdb3f2f
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-02-23 17:34:47 -0300

    feat: Created test for create_user service object. It test if CreateUser creates a guess user.

---
+++ spec/services/create_user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+describe ".call" do
+  it "should create user guest" do
+    user_params = create(:user, guest: true)
+    cart = create(:cart, user: user_params)
+
+    user = CreateUser.call(cart, user_params)
+
+    expect(user.class).to eq(User)
+    expect(user.attributes).to include({ "id"=>1,
+                                         "email"=>"user@spec.io",
+                                         "first_name"=>"John",
+                                         "last_name"=>"Doe",
+                                         "guest"=>true }
+                                       )
+  end
+end
