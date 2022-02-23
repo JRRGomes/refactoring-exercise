94c76b1836b7dacbf5b11db279582632c84e79ee
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-02-23 14:38:58 -0300

    style: Fixed identation ot the tests

--- spec/services/create_order_spec.rb
+++ spec/services/create_order_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
 describe ".call" do
-    it "should create order with the user and address attributes" do
-      user = create(:user)
-      address_params = create(:order, user: user)
+  it "should create order with the user and address attributes" do
+    user = create(:user)
+    address_params = create(:order, user: user)
 
-      order = CreateOrder.call(user, address_params)
+    order = CreateOrder.call(user, address_params)
 
-      expect(order.class).to eq(Order)
-      expect(order.attributes).to include({ "id"=>2,
-                                            "user_id"=>1,
-                                            "first_name"=>user.first_name,
-                                            "last_name"=>user.last_name,
-                                            "address_1"=>nil,
-                                            "address_2"=>nil,
-                                            "city"=>nil,
-                                            "state"=>nil,
-                                            "country"=>nil,
-                                            "zip"=>nil }
-                                         )
-    end
+    expect(order.class).to eq(Order)
+    expect(order.attributes).to include({ "id"=>2,
+                                          "user_id"=>1,
+                                          "first_name"=>user.first_name,
+                                          "last_name"=>user.last_name,
+                                          "address_1"=>nil,
+                                          "address_2"=>nil,
+                                          "city"=>nil,
+                                          "state"=>nil,
+                                          "country"=>nil,
+                                          "zip"=>nil }
+                                        )
+  end
 end
