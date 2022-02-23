20c84aa21ca1d022ab97c2e8ed985b96f9bb6dd3
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-02-22 21:11:10 -0300

    feat: Created another service object to create new Order Line Item, in order to reduce the responsability of the purchases controller

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -21,13 +21,7 @@ class PurchasesController < ApplicationController
 
         cart.items.each do |item|
           item.quantity.times do
-            order.items << OrderLineItem.new(
-              order: order,
-              sale: item.sale,
-              unit_price_cents: item.sale.unit_price_cents,
-              shipping_costs_cents: shipping_costs,
-              paid_price_cents: item.sale.unit_price_cents + shipping_costs
-            )
+            order.items << CreateOrderLineItem.call(order, item, shipping_costs)
           end
         end
 
