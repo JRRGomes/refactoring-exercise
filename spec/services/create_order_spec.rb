bcf9713b76076772b5f9fea937ac7fab7293036e
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-02-24 15:58:50 -0300

    fix: Fixed the address_params values in order to test the CreateOrder properly

--- spec/services/create_order_spec.rb
+++ spec/services/create_order_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
-describe ".call" do
-  it "should create order with the user and address attributes" do
+describe '.call' do
+  it 'should create order with the user and address attributes' do
     user = create(:user)
-    address_params = create(:order, user: user)
+    address_params = { address_1: '12 Grimmauld Place', address_2: '4 Privet Drive' }
 
     order = CreateOrder.call(user, address_params)
 
     expect(order.class).to eq(Order)
-    expect(order.attributes).to include({ "id"=>2,
-                                          "user_id"=>1,
-                                          "first_name"=>user.first_name,
-                                          "last_name"=>user.last_name,
-                                          "address_1"=>nil,
-                                          "address_2"=>nil,
-                                          "city"=>nil,
-                                          "state"=>nil,
-                                          "country"=>nil,
-                                          "zip"=>nil }
-                                       )
+    expect(order.attributes).to include({ 'id'=>1,
+      'user_id'=>1,
+      'first_name'=>user.first_name,
+      'last_name'=>user.last_name,
+      'address_1'=>'12 Grimmauld Place',
+      'address_2'=>'4 Privet Drive',
+      'city'=>nil,
+      'state'=>nil,
+      'country'=>nil,
+      'zip'=>nil }
+      )
   end
 end
