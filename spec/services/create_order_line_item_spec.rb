043916b4b93de566deefb17fe52d208bea79bf94
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-02-24 20:44:45 -0300

    style: Fixed identations and quotes

--- spec/services/create_order_line_item_spec.rb
+++ spec/services/create_order_line_item_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-describe ".call" do
-  it "should create order line item with the order, item and shipping_costs attributes" do
+describe '.call' do
+  it 'create order line item with the order, item and shipping_costs attributes' do
     order = create(:order)
     cart = create(:cart, user: order.user)
     item = create(:cart_item, cart: cart)
@@ -10,11 +10,11 @@ describe ".call" do
     order_line_item  = CreateOrderLineItem.call(order, item, shipping_costs)
 
     expect(order_line_item.class).to eq(OrderLineItem)
-    expect(order_line_item.attributes).to include({ "order_id"=>order.id,
-                                                    "sale_id"=>item.sale.id,
-                                                    "unit_price_cents"=>item.sale.unit_price_cents,
-                                                    "shipping_costs_cents"=>shipping_costs,
-                                                    "paid_price_cents"=>item.sale.unit_price_cents + shipping_costs }
-                                                  )
-    end
+    expect(order_line_item.attributes).to include({ 'order_id'=>order.id,
+      'sale_id'=>item.sale.id,
+      'unit_price_cents'=>item.sale.unit_price_cents,
+      'shipping_costs_cents'=>shipping_costs,
+      'paid_price_cents'=>item.sale.unit_price_cents + shipping_costs }
+      )
+  end
 end
--- spec/services/create_user_spec.rb
+++ spec/services/create_user_spec.rb
@@ -5,9 +5,9 @@ describe ".call" do
     it "creates user guest" do
       cart = create(:cart, user: nil)
       user_params = { 'email'=>'teste@spec.io', 'first_name'=>'John', 'last_name'=>'Doe'}
-      
+
       user = CreateUser.call(cart: cart, purchase_params: { user: user_params })
-      
+
       expect(user.class).to eq(User)
       expect(user.attributes).to include({ 'email'=>'teste@spec.io',
         'first_name'=>'John',
