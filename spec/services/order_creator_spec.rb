e9da531ce0a7c7c829e2fa1f32bc88bd7164bdc7
Jefferson Gomes <jeffersonrolim@gmail.com>
2022-02-25 08:34:30 -0300

    style: Added some spaces on hash declarations

--- spec/services/order_creator_spec.rb
+++ spec/services/order_creator_spec.rb
@@ -9,16 +9,16 @@ describe '.call' do
       order = OrderCreator.call(user, address_params)
 
       expect(order.class).to eq(Order)
-      expect(order.attributes).to include({ 'id'=>1,
-        'user_id'=>1,
-        'first_name'=>user.first_name,
-        'last_name'=>user.last_name,
-        'address_1'=>'12 Grimmauld Place',
-        'address_2'=>'4 Privet Drive',
-        'city'=>nil,
-        'state'=>nil,
-        'country'=>nil,
-        'zip'=>nil }
+      expect(order.attributes).to include({ 'id' => 1,
+        'user_id' => 1,
+        'first_name' => user.first_name,
+        'last_name' => user.last_name,
+        'address_1' => '12 Grimmauld Place',
+        'address_2' => '4 Privet Drive',
+        'city' => nil,
+        'state' => nil,
+        'country' => nil,
+        'zip' => nil }
         )
     end
   end
--- spec/services/user_creator_spec.rb
+++ spec/services/user_creator_spec.rb
@@ -2,17 +2,17 @@ require 'rails_helper'
 
 describe ".call" do
   context 'when the cart does not has a user' do
+    let(:cart) { create(:cart, user: nil) }
     it "creates user guest" do
-      cart = create(:cart, user: nil)
-      user_params = { 'email'=>'teste@spec.io', 'first_name'=>'John', 'last_name'=>'Doe'}
-
+      user_params = { 'email' => 'user@spec.io', 'first_name' => 'John', 'last_name' => 'Doe' }
+      
       user = UserCreator.call(cart: cart, purchase_params: { user: user_params })
 
       expect(user.class).to eq(User)
-      expect(user.attributes).to include({ 'email'=>'teste@spec.io',
-        'first_name'=>'John',
-        'last_name'=>'Doe',
-        'guest'=>true }
+      expect(user.attributes).to include({ 'email' => 'user@spec.io',
+        'first_name' => 'John',
+        'last_name' => 'Doe',
+        'guest' => true }
         )
     end
   end
